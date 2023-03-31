743b4b14150980161a6aefe4c2732d877322b498
Retr02605 <117747650+Retr02605@users.noreply.github.com>
2023-03-31 11:18:55 -0500

    proyecto arreglado

--- scripts/script.js
+++ scripts/script.js
@@ -32,6 +32,73 @@ function active() {
       like.classList.remove("card-like");
     }
 }
+
+let likeTwo = document.getElementById("heart-like-two");
+likeTwo.addEventListener("click", activeTwo);
+
+function activeTwo() {
+    if (likeTwo.classList.contains("card_like-active")) {
+      likeTwo.classList.add("card-like");
+      likeTwo.classList.remove("card_like-active");
+    } else {
+      likeTwo.classList.add("card_like-active");
+      likeTwo.classList.remove("card-like");
+    }
+}
+
+let likeThree = document.getElementById("heart-like-three");
+likeThree.addEventListener("click", activeThree);
+
+function activeThree() {
+    if (likeThree.classList.contains("card_like-active")) {
+      likeThree.classList.add("card-like");
+      likeThree.classList.remove("card_like-active");
+    } else {
+      likeThree.classList.add("card_like-active");
+      likeThree.classList.remove("card-like");
+    }
+}
+
+let likefour = document.getElementById("heart-like-four");
+likefour.addEventListener("click", activeFour);
+
+function activeFour() {
+    if (likefour.classList.contains("card_like-active")) {
+      likefour.classList.add("card-like");
+      likefour.classList.remove("card_like-active");
+    } else {
+      likefour.classList.add("card_like-active");
+      likefour.classList.remove("card-like");
+    }
+}
+
+let likefive = document.getElementById("heart-like-five");
+likefive.addEventListener("click", activeFive);
+
+function activeFive() {
+    if (likefive.classList.contains("card_like-active")) {
+      likefive.classList.add("card-like");
+      likefive.classList.remove("card_like-active");
+    } else {
+      likefive.classList.add("card_like-active");
+      likefive.classList.remove("card-like");
+    }
+}
+
+let likesix = document.getElementById("heart-like-six");
+likesix.addEventListener("click", activeSix);
+
+function activeSix() {
+    if (likesix.classList.contains("card_like-active")) {
+      likesix.classList.add("card-like");
+      likesix.classList.remove("card_like-active");
+    } else {
+      likesix.classList.add("card_like-active");
+      likesix.classList.remove("card-like");
+    }
+}
+
+
 function save() {
     namess.textContent = nameInput.value;
     profesion.textContent=profesionInput.value
